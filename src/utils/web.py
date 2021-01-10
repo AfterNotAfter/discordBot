0d6e676d3a1cc663821b8198770c7602f446ef78
K.DH3 <30466064+331leo@users.noreply.github.com>
2021-01-11 01:13:10 +0900

    feat: async_screenshot

--- src/utils/web.py
+++ src/utils/web.py
@@ -2,8 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import asyncio
 import time
-def screenshot(url):
+import arsenic 
+async def screenshot(url):
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     driver = webdriver.Chrome(chrome_options=options)
@@ -11,11 +13,28 @@ def screenshot(url):
     driver.set_window_size(600,1000)
     driver.get(url)
     print("start waiting...")
-    time.sleep(1)
+    time.sleep(2)
     print("taking screenshot")
     driver.save_screenshot("screenshot.png")
     with open("screenshot.png", 'rb') as f:
         b = f.read()
     print("finish")
     return b
+    
+async def async_screenshot(url):
+    service = arsenic.services.Chromedriver()
+    browser = arsenic.browsers.Chrome()
+    browser.capabilities = {
+        "goog:chromeOptions": {"args": ["--headless", "--disable-gpu"]}
+    }
+    #service = arsenic.services.Geckodriver()
+    #browser = arsenic.browsers.Firefox()
+    async with arsenic.get_session(service, browser) as session:
+        await session.set_window_size(600,1000)
+        await session.get(url)
+        await asyncio.sleep(2)
+        a=await session.get_screenshot()
+        
+    print("finish")
+    return a
     
